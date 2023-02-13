600ec6a8d884597098121e614e78f14e1b38014c
Angel Jaramillo <angeljara07@outlook.com>
2023-02-13 00:01:00 -0500

    importando paquete de lectura para código QR

--- lib/widgets/scan_button.dart
+++ lib/widgets/scan_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class ScanButton extends StatelessWidget {
   const ScanButton({super.key});
@@ -8,7 +9,9 @@ class ScanButton extends StatelessWidget {
     return FloatingActionButton(
       elevation: 0,
       child: const Icon(Icons.filter_center_focus, color: Colors.white,),
-      onPressed: () {},
+      onPressed: () async {
+        String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode("·3D8BEF", "Cancelar", false, ScanMode.QR);
+      },
     );
   }
 }
