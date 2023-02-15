ad9b9c2983fbfe16ddf5a732b88f026090414a97
Angel <angeljara07@outlook.com>
2023-02-14 21:19:26 -0500

    corrigiendo error android gradle

--- lib/widgets/scan_button.dart
+++ lib/widgets/scan_button.dart
@@ -10,7 +10,7 @@ class ScanButton extends StatelessWidget {
       elevation: 0,
       child: const Icon(Icons.filter_center_focus, color: Colors.white,),
       onPressed: () async {
-        String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode("·3D8BEF", "Cancelar", false, ScanMode.QR);
+        String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode("#3D8BEF", "Cancelar", false, ScanMode.QR);
       },
     );
   }
